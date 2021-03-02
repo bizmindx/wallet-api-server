c2e48cf1121809f6a14b9e1d94a5bb04c4d358e0
codaelux <bizmindx@gmail.com>
2021-03-02 17:46:09 +0100

    rate limit and compression added

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,23 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import rateLimit from 'express-rate-limit';
+import compression from 'compression';
+import helmet from 'helmet';
 import cors from 'cors';
 import routes from '../api';
 import config from '../config';
-import { Request, Response, NextFunction, ErrorRequestHandler} from 'express';
 
+const Limiter = rateLimit({ max: 20, windowMs: 1 * 60 * 1000, message: 'IP has exceed request threshold' });
 
 export default ({ app }: { app: express.Application }) => {
   app.get('/status', (req, res) => {
     res.status(200).json({message: 'server is up and running'});
   });
-
-
+  
   app.enable('trust proxy');
+  app.use(compression())
+  app.use(helmet());
   app.use(cors());
-
+  app.use(Limiter);
   app.use(bodyParser.json());
   app.use(config.api.prefix, routes());
-
   app.use((req, res, next) => {
     const err: any = new Error('Not Found');
     err['status'] = 404;
